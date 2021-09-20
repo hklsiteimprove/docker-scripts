8b0a8184bd4c6fb11a55b638b14724334a84ee4a
Henrik Klarup <hkl@siteimprove.com>
2021-09-20 11:06:20 +0200

    Add mount as path

--- docker-to-dot.sh
+++ docker-to-dot.sh
@@ -7,11 +7,10 @@ declare -a OUTPUTLINES=()
 while read -r line ; do
     FROM=`echo $line | sed -e 's/FROM //ig' | sed -e 's/ AS.*//ig'`
     if [[ "${line,,}" == *" as "* ]]; then
-    	TO=`echo $line | sed -e 's/.*as //ig'`
+        TO=`echo $line | sed -e 's/.*as //ig'`
     else
-    	TO="final"
+        TO="final"
     fi
-
     output="\"$FROM\" -> \"$TO\";"
     OUTPUTLINES+=("$output")
 done <<<$(grep -i "from" $1 | grep -vi "from=" | sed -e 's/FROM //ig' | cat)
@@ -20,14 +19,17 @@ done <<<$(grep -i "from" $1 | grep -vi "from=" | sed -e 's/FROM //ig' | cat)
 CURRENT_STAGE=0
 while read -r line ; do
     if [[ "${line,,}" == *" as "* ]]; then
-    	CURRENT_STAGE=`echo $line | sed -e 's/.*as //ig'`
+        CURRENT_STAGE=`echo $line | sed -e 's/.*as //ig'`
     elif [[ "${line,,}" == *"from "* ]]; then
-	CURRENT_STAGE="final"
+        CURRENT_STAGE="final"
     fi
 
     if [[ "${line,,}" == *"copy --from="* ]]; then
-	FROM=`echo $line | sed -e 's/COPY --from=//ig' | sed -e 's/ .*//ig'`
-	OUTPUTLINES+=("\"$FROM\" -> \"$CURRENT_STAGE\";")
+        FROM=`echo $line | sed -e 's/COPY --from=//ig' | sed -e 's/ .*//ig'`
+        OUTPUTLINES+=("\"$FROM\" -> \"$CURRENT_STAGE\";")
+    elif [[ "${line,,}" == *"mount=type=bind,from="* ]]; then
+        FROM=`echo $line | sed -e 's/.*from=//ig' | sed -e 's/,.*//ig'`
+        OUTPUTLINES+=("\"$FROM\" -> \"$CURRENT_STAGE\";")
     fi
 done <<<$(cat $1)
 
